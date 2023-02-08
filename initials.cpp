ca06eb5eb36dfc61aa27558d188e830b900f97ba
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-02-08 16:04:50 -0500

    Add files via upload

---
+++ initials.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  std :: cout << "H        H         A       \n";
+  std :: cout << "H        H        A A      \n";
+  std :: cout << "H        H       A   A     \n";
+  std :: cout << "H        H      A     A    \n";
+  std :: cout << "H        H     A       A   \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "HHHHHHHHHH    AAAAAAAAAAA  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  std :: cout << "H        H    A         A  \n";
+  
+}

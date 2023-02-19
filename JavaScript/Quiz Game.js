2c49ce3a7f862e89cf52e7bd55771b6dab4953d6
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-02-18 23:29:50 -0500

    Update Quiz Game.js

--- JavaScript/Quiz Game.js
+++ JavaScript/Quiz Game.js
@@ -18,4 +18,69 @@
       },
       {
         question: "What is the largest country in the world?",
-        choices:
+        choices: ["China", "United States", "Russia", "Brazil"],
+        answer: "Russia"
+      },
+      {
+        question: "What is the currency of Japan?",
+        choices: ["Yuan", "Won", "Yen", "Dollar"],
+        answer: "Yen"
+      }
+    ];
+
+    const questionDiv = document.getElementById("question");
+    const choicesList = document.getElementById("choices");
+    const submitButton = document.getElementById("submit");
+    const resultDiv = document.getElementById("result");
+
+    let currentQuestion = 0;
+    let score = 0;
+
+    function displayQuestion() {
+      const question = questions[currentQuestion];
+      questionDiv.textContent = question.question;
+
+      choicesList.innerHTML = "";
+      question.choices.forEach(choice => {
+        const li = document.createElement("li");
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "choice";
+        radio.value = choice;
+        li.appendChild(radio);
+        li.appendChild(document.createTextNode(choice));
+        choicesList.appendChild(li);
+      });
+    }
+
+    function checkAnswer() {
+      const selectedChoice = document.querySelector('input[name="choice"]:checked');
+      if (!selectedChoice) return;
+
+      const answer = selectedChoice.value;
+      if (answer === questions[currentQuestion].answer) {
+        resultDiv.textContent = "Correct!";
+        score++;
+      } else {
+        resultDiv.textContent = "Incorrect.";
+      }
+
+      currentQuestion++;
+      if (currentQuestion >= questions.length) {
+        endGame();
+      } else {
+        displayQuestion();
+      }
+    }
+
+    function endGame() {
+      questionDiv.textContent = `You scored ${score} out of ${questions.length}.`;
+      choicesList.innerHTML = "";
+      submitButton.style.display = "none";
+    }
+
+    displayQuestion();
+    submitButton.addEventListener("click", checkAnswer);
+  </script>
+</body>
+</html>

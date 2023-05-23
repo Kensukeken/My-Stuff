0574070531d7b7599e3290f1daeb891feea68b69
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2023-05-23 16:45:44 -0400

    Update  main.cpp

--- C++/bank / main.cpp
+++ C++/bank / main.cpp
@@ -7,77 +7,79 @@ double withdraw(double saveBal);
 void printBalance(double saveBal);
 
 int main() {
-  // All the variables you need
-  string name = "John"; // Add your name
+  string name = "Haya";
   double savings = 1000;
   int choice1, choice2;
-  bool access = true; // Needs to be equal to true
+  bool access = true;
 
   while (access) {
     cout << name << ", how can we help you today?\n";
     cout << "Enter the number for your choice:\n";
     cout << "1. Deposit\n";
     cout << "2. Withdraw\n";
-    cout << "3. Print balance of all accounts.\n";
-    cin >> choice1;
+    cout << "3. Print balance of all accounts\n";
+
+    cin >> choice1; // User inputs the choice
 
     if (choice1 == 1) {
       cout << "You chose to deposit.\n";
-      savings = deposit(savings);
-    } else if (choice1 == 2) {
+      savings = deposit(savings); // Call deposit function and update savings balance
+    }
+    else if (choice1 == 2) {
       cout << "You chose to withdraw.\n";
-      savings = withdraw(savings);
-    } else if (choice1 == 3) {
+      savings = withdraw(savings); // Call withdraw function and update savings balance
+    }
+    else if (choice1 == 3) {
       cout << "You chose to print balances.\n";
-      printBalance(savings);
-    } else {
-      cout << "Invalid choice.\n";
+      printBalance(savings); // Call printBalance function to display savings balance
+    }
+    else {
+      cout << "Invalid choice.\n"; // Displayed when an invalid choice is entered
     }
 
     cout << "Would you like to do another transaction?\n";
     cout << "Enter 1 for yes.\n";
     cout << "Enter 2 for no.\n";
-    cin >> choice2;
+    cin >> choice2; // User inputs choice for another transaction
 
     if (choice2 == 1) {
-      cout << "You chose yes!\n";
-    } else if (choice2 == 2) {
+      cout << "You chose yes!\n"; // Displayed if the user chooses to continue
+    }
+    else if (choice2 == 2) {
       cout << "You chose no.\n";
-      access = false;
-      cout << "Have a nice rest of the day!\n" << endl;
-    } else {
-      cout << "Invalid choice.\n";
+      access = false; // Set access variable to false to exit the while loop
+      cout << "Have a nice rest of the day!\n" << endl; // Display farewell message
     }
   }
-  
-  return 0;
+
+  return 0; // Exit the program
 }
 
 double deposit(double saveBal) {
   double depositAmt;
-  cout << "Enter the $ amount you would like to deposit:\n";
-  cin >> depositAmt;
-  saveBal += depositAmt;
-  cout << "Your new savings balance is: " << saveBal << "\n";
-  return saveBal;
+  cout << "Enter the amount you would like to deposit: $";
+  cin >> depositAmt; // User inputs the deposit amount
+  saveBal += depositAmt; // Add the deposit amount to the savings balance
+  cout << "Your new savings balance is: $" << saveBal << "\n"; // Display the updated savings balance
+  return saveBal; // Return the new savings balance
 }
 
 double withdraw(double saveBal) {
   double withdrawAmt;
-  cout << "Enter the $ amount you would like to withdraw:\n";
-  cin >> withdrawAmt;
+  cout << "Enter the amount you would like to withdraw: $";
+  cin >> withdrawAmt; // User inputs the withdrawal amount
 
   if (withdrawAmt > saveBal) {
-    cout << "You do not have enough funds for this withdrawal.\n";
-    return saveBal;
+    cout << "You do not have enough funds for this withdrawal.\n"; // Displayed if withdrawal amount exceeds savings balance
+    return saveBal; // Return the original savings balance
   }
 
-  saveBal -= withdrawAmt;
-  cout << "Your new savings balance is: " << saveBal << "\n";
-  return saveBal;
+  saveBal -= withdrawAmt; // Subtract the withdrawal amount from the savings balance
+  cout << "Your new savings balance is: $" << saveBal << "\n"; // Display the updated savings balance
+  return saveBal; // Return the new savings balance
 }
 
 void printBalance(double saveBal) {
   cout << "Account Balance:\n";
-  cout << "Savings balance: $" << saveBal << "\n";
+  cout << "Savings balance: $" << saveBal << "\n"; // Display the savings balance
 }

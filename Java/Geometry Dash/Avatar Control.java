d430c5980316314274b1ca2c1d22afa033090737
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-12-12 15:52:23 -0500

    Create Avatar Control.java

---
+++ Java/Geometry Dash/Avatar Control.java
@@ -0,0 +1,7 @@
+frame.addKeyListener(new KeyAdapter() {
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            // Handle jump
+        }
+    }
+});

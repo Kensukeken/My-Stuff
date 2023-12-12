712d4e1bf322cddf24a8762beaecf003ce2f15f8
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-12-12 15:51:58 -0500

    Create Obstacle.java

---
+++ Java/Geometry Dash/Obstacle.java
@@ -0,0 +1,10 @@
+public class Obstacle {
+    int x, y; // Position of the obstacle
+
+    public Obstacle(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Other methods to handle obstacle behavior
+}

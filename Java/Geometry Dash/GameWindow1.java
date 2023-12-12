69453de5a0d0f7ca25344583911a5cc3deb20489
Akira <85637598+Kensukeken@users.noreply.github.com>
2023-12-12 15:51:42 -0500

    Create GameWindow1.java

---
+++ Java/Geometry Dash/GameWindow1.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GameWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Geometry Dash Game");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}

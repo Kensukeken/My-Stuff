5792f0cfb4b7dfaab1b14fea3cb5a1b0a4dd0187
Kenzy <85637598+Kensukeken@users.noreply.github.com>
2024-01-15 21:59:18 -0500

    Create Sound.java

---
+++ Java/Sound.java
@@ -0,0 +1,17 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Sound {
+    public static void playSound(String filePath) {
+        try {
+            File soundFile = new File(filePath);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
